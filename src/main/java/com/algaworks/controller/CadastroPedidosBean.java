2836343aebd58a8efab8ea9e8c00a739fa06384a
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-06-14 05:15:31 -0300

    Prototipando o Cadastro de Pedidos

---
+++ src/main/java/com/algaworks/controller/CadastroPedidosBean.java
@@ -0,0 +1,23 @@
+package com.algaworks.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CadastroPedidosBean {
+
+	private List<Integer> itens;
+	
+	public CadastroPedidosBean() {
+		itens = new ArrayList<>();
+		itens.add(1);
+	}
+
+	public List<Integer> getItens() {
+		return itens;
+	}
+}
