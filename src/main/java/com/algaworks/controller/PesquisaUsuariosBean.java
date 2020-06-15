0d52e2314d732bce86143ff9a15d5768dcbce1d4
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-06-15 02:28:36 -0300

    Desafio: Prototipando a Pesquisa de Usuários

---
+++ src/main/java/com/algaworks/controller/PesquisaUsuariosBean.java
@@ -0,0 +1,25 @@
+package com.algaworks.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaUsuariosBean {
+
+	private List<Integer> usuariosFiltrados;
+	
+	public PesquisaUsuariosBean() {
+		usuariosFiltrados = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			usuariosFiltrados.add(i);
+		}
+	}
+
+	public List<Integer> getUsuariosFiltrados() {
+		return usuariosFiltrados;
+	}
+}
