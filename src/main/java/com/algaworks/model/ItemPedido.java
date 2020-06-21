9de03b0e5d2ee43fe7632590f2b702f371afe265
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-06-21 00:44:24 -0300

    Mapeando Entidades do Pedido de Venda

--- src/main/java/com/algaworks/model/ItemPedido.java
+++ src/main/java/com/algaworks/model/ItemPedido.java
@@ -3,17 +3,29 @@ package com.algaworks.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "itens_pedido")
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue
 	private Long id;
 	
+	@Column(nullable = false, length = 3)
 	private Integer quantidade;
 	
+	@Column(name = "valor_unitario", nullable = false, precision = 10, scale = 2)
 	private BigDecimal valorUnitario;
 	
+	@ManyToOne
+	@JoinColumn(name = "produto_id", nullable = false)
 	private Produto produto;
 	
+	@ManyToOne
+	@JoinColumn(name = "pedido_id", nullable = false)
 	private Pedido pedido;
 
 	public Long getId() {
