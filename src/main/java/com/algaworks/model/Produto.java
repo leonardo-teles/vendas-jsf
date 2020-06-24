329aa35f50fc23f0acc991942fa425ad48b3f491
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-06-23 22:26:56 -0300

    Adicionando Restrições no Cadastro de Produtos

--- src/main/java/com/algaworks/model/Produto.java
+++ src/main/java/com/algaworks/model/Produto.java
@@ -13,6 +13,9 @@ import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @Table(name = "produtos")
@@ -23,19 +26,26 @@ public class Produto implements Serializable {
 	@GeneratedValue
 	private Long id;
 	
+	@NotBlank
+	@Size(max = 80)
 	@Column(nullable = false, length = 80)
 	private String nome;
 	
+	@NotBlank
 	@Column(nullable = false, length = 20, unique = true)
 	private String sku;
 	
+	@NotNull
 	@Column(name = "valor_unitario", nullable = false, precision = 10, scale = 2)
 	private BigDecimal valorUnitario;
 	
-	@NotNull @Min(0) @Max(9999)
+	@NotNull 
+	@Min(0) 
+	@Max(9999)
 	@Column(nullable = false, length = 3)
 	private Integer quantidadeEstoque;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "categoria_id", nullable = false)
 	private Categoria categoria;
