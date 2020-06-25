f67dde26426ad4cd78e1c483ec3e19082b32e597
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-06-25 16:15:56 -0300

    Implementando a Pesquisa de Produtos Usando Criteria API do JPA

--- src/main/java/com/algaworks/controller/PesquisaProdutosBean.java
+++ src/main/java/com/algaworks/controller/PesquisaProdutosBean.java
@@ -1,27 +1,40 @@
 package com.algaworks.controller;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import com.algaworks.model.Produto;
+import com.algaworks.repository.ProdutoRepository;
+import com.algaworks.repository.filter.ProdutoFilter;
 
 @Named
 @ViewScoped
 public class PesquisaProdutosBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private List<Integer> produtosFiltrados;
+	@Inject
+	private ProdutoRepository produtoRepository;
+	
+	private ProdutoFilter filtro;
+	private List<Produto> produtosFiltrados;
 	
 	public PesquisaProdutosBean() {
-		produtosFiltrados = new ArrayList<>();
-		for (int i = 0; i < 50; i++) {
-			produtosFiltrados.add(i);
-		}
+		filtro = new ProdutoFilter();
+	}
+	
+	public void pesquisar() {
+		produtosFiltrados = produtoRepository.produtosFiltrados(filtro);
+	}
+	
+	public List<Produto> getProdutosFiltrados() {
+		return produtosFiltrados;
 	}
 
-	public List<Integer> getProdutosFiltrados() {
-		return produtosFiltrados;
+	public ProdutoFilter getFiltro() {
+		return filtro;
 	}
 }
--- src/main/java/com/algaworks/repository/ProdutoRepository.java
+++ src/main/java/com/algaworks/repository/ProdutoRepository.java
@@ -1,12 +1,25 @@
 package com.algaworks.repository;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Fetch;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.algaworks.model.Categoria;
 import com.algaworks.model.Produto;
+import com.algaworks.repository.filter.ProdutoFilter;
 
 public class ProdutoRepository implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -27,4 +40,48 @@ public class ProdutoRepository implements Serializable {
 			return null;
 		}
 	}
+	
+	public List<Produto> produtosFiltrados(ProdutoFilter filtro) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Produto> criteriaQuery = builder.createQuery(Produto.class);
+		List<Predicate> predicates = new ArrayList<>();
+		
+		Root<Produto> produtoRoot = criteriaQuery.from(Produto.class);
+		Fetch<Produto, Categoria> categoriaJoin =  produtoRoot.fetch("categoria", JoinType.INNER);
+		categoriaJoin.fetch("categoriaPai", JoinType.INNER);
+		
+		if (StringUtils.isNotBlank(filtro.getSku())) {
+			predicates.add(builder.equal(produtoRoot.get("sku"), filtro.getSku()));
+		}
+			  
+		if (StringUtils.isNotBlank(filtro.getNome())) {
+			predicates.add(builder.like(builder.lower(produtoRoot.get("nome")), "%" + filtro.getNome().toLowerCase() + "%"));
+		}
+		
+		criteriaQuery.select(produtoRoot);
+		criteriaQuery.where(predicates.toArray(new Predicate[0]));
+		criteriaQuery.orderBy(builder.asc(produtoRoot.get("nome")));
+		
+		TypedQuery<Produto> query = manager.createQuery(criteriaQuery);
+		
+		return query.getResultList();
+	}
+	
+	/*
+	 * @SuppressWarnings("unchecked") public List<Produto>
+	 * produtosFiltrados(ProdutoFilter filtro) { Session session =
+	 * manager.unwrap(Session.class);
+	 * 
+	 * @SuppressWarnings("deprecation") Criteria criteria =
+	 * session.createCriteria(Produto.class);
+	 * 
+	 * if (StringUtils.isNotBlank(filtro.getSku())) {
+	 * criteria.add(Restrictions.eq("sku", filtro.getSku())); }
+	 * 
+	 * if (StringUtils.isNotBlank(filtro.getNome())) {
+	 * criteria.add(Restrictions.ilike("nome", filtro.getNome(),
+	 * MatchMode.ANYWHERE)); }
+	 * 
+	 * return criteria.addOrder(Order.asc("nome")).list(); }
+	 */
 }
