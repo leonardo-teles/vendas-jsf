08ac8f6446c3529db764a38d76811fc6d7733b03
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-07-08 01:49:25 -0300

    Controlando Status de Pedidos

--- src/main/java/com/algaworks/model/Pedido.java
+++ src/main/java/com/algaworks/model/Pedido.java
@@ -277,6 +277,16 @@ public class Pedido implements Serializable {
 		return StatusPedido.CANCELADO.equals(this.getStatus());
 	}
 
+	@Transient
+	public boolean isNaoAlteravel() {
+		return !this.isAlteravel();
+	}
+
+	@Transient
+	private boolean isAlteravel() {
+		return this.isOrcamento();
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
