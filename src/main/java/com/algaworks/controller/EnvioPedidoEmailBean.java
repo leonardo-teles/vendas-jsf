13f6481e6360368f05e29856c3011836c5986739
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-07-09 18:04:21 -0300

    Template de e-Mail com Apache Velocity

--- src/main/java/com/algaworks/controller/EnvioPedidoEmailBean.java
+++ src/main/java/com/algaworks/controller/EnvioPedidoEmailBean.java
@@ -1,16 +1,22 @@
 package com.algaworks.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Locale;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.apache.velocity.tools.generic.NumberTool;
 
 import com.algaworks.model.Pedido;
 import com.algaworks.util.jsf.FacesUtil;
 import com.algaworks.util.mail.Mailer;
 import com.algaworks.validation.PedidoEdicao;
 import com.outjected.email.api.MailMessage;
+import com.outjected.email.impl.templating.velocity.VelocityTemplate;
 
 @Named
 @RequestScoped
@@ -24,14 +30,19 @@ public class EnvioPedidoEmailBean implements Serializable {
 	@PedidoEdicao
 	private Pedido pedido;
 	
-	public void enviarPedido() {
+	public void enviarPedido() throws IOException {
 		MailMessage message = mailer.novaMensagem();
+		
+		String path = getClass().getClassLoader().getResource("emails/pedido.template").getFile();
 		
 		message.to(this.pedido.getCliente().getEmail())
 			.subject("Pedido " + this.pedido.getId())
-			.bodyHtml("<strong>Valor total:</strong> " + this.pedido.getValorTotal())
+			.bodyHtml(new VelocityTemplate(new File(path)))
+			.put("pedido", this.pedido)
+			.put("numberTool", new NumberTool())
+			.put("locale", new Locale("pt", "BR"))
 			.send();
-		
+	
 		FacesUtil.addInfoMessage("Pedido enviado por e-mail com sucesso.");
 	}
 }
