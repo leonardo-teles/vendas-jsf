d142e0c5e14abab84b2a93c5d9ebcf4375dd089a
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-07-24 14:51:48 -0300

    Exportando para Excel com DataExporter

--- src/main/java/com/algaworks/controller/PesquisaPedidosBean.java
+++ src/main/java/com/algaworks/controller/PesquisaPedidosBean.java
@@ -8,6 +8,14 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+
 import com.algaworks.model.Pedido;
 import com.algaworks.model.StatusPedido;
 import com.algaworks.repository.PedidoRepository;
@@ -39,6 +47,28 @@ public class PesquisaPedidosBean implements Serializable {
 	public StatusPedido[] getStatus() {
 		return StatusPedido.values();
 	}
+	
+	//estilização do arquivo XLS
+	public void posProcessarXLS(Object documento) {
+		HSSFWorkbook planilha = (HSSFWorkbook) documento;
+		HSSFSheet folha = planilha.getSheetAt(0);
+		HSSFRow cabecalho = folha.getRow(0);
+		HSSFCellStyle estiloCelula = planilha.createCellStyle();
+		Font fonteCabecalho = planilha.createFont();
+		
+		fonteCabecalho.setColor(IndexedColors.WHITE.getIndex());
+		fonteCabecalho.setBold(true);
+		fonteCabecalho.setFontHeightInPoints((short) 14);
+		
+		estiloCelula.setFont(fonteCabecalho);
+		estiloCelula.setFillBackgroundColor(IndexedColors.BLACK.getIndex());
+		estiloCelula.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		
+		for(int i = 0; i < cabecalho.getPhysicalNumberOfCells(); i++) {
+			cabecalho.getCell(i).setCellStyle(estiloCelula);
+		}
+	}	
+		
 
 	public List<Pedido> getPedidosFiltrados() {
 		return pedidosFiltrados;
